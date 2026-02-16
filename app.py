467ecac87d1708fce5706bd87b5ad205a3ac0a8c
Nikhitha V <nikhithav8@gmail.com>
2026-02-16 20:03:39 +0530

    Updated feature / fixed bug

--- app.py
+++ app.py
@@ -1217,5 +1217,7 @@ def get_student_details(roll_no):
     }
     
     return jsonify({'success': True, 'student': student_data})
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
